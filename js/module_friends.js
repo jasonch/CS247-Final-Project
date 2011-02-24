4a73049c6ff363c515d743a94417e801ed831808
Chun-Chang Chen <jasonch@stanford.edu>
2011-02-23 22:52:16 -0800

    style changes, friendbox onclick

--- js/module_friends.js
+++ js/module_friends.js
@@ -1,17 +1,30 @@
 function load_friends () {
+    var searchbox = 
+      "<div class='friends-search'>" +
+      "   <input type='text' id='friends-search-box' value='search...' onclick='clearText(this);'></input>" + 
+      "</div>";
+
 	  FB.api ('/me/friends', function (friends) {
-	    window.FRIENDS = friends.data;
 	    var result = "";
 	    for(var i = 0; i < friends.data.length && i < 20; i++) {
-	      result += formatFriendItem(friends.data[i]);
+	      result += formatFriendItem(friends.data[i], friends.data[i].id);
 	    }
-	    $('#block-friends').html (result);
-      $('#block-friends').show ();
+	    $('#block-friends').html (searchbox + result);
+
+      for (var i = 0; i < friends.data.length && i < 20; i++) {
+        var id = friends.data[i].id;
+        $('#friend-item-' + id).bind ('click', {fid:id}, function (event) {
+          window.open (AJAX_DIR + "challengeFriend.php?from="+USER_INFO.user_id + "&to=" + event.data.fid, 
+            "Challenge A Friend", "status=0,toolbar=0,menubar=0,resizable=0,height=300,width=300");
+        });
+      }
+
 	  });
 }
 
-function formatFriendItem(f) {
-  return "<div class='friend-item'>"+
+function formatFriendItem(f, id) {
+
+  return "<div class='friend-item' id='friend-item-"+id+"'>"+
     "<div class='user_pic'><img src='http://graph.facebook.com/" + 
     f.id + "/picture'/></div><a href='#'>"+f.name+"</a></div>";
 }
