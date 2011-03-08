872297151a33715839cbb4266fac3798383663a9
Darius Liddell <djtrack16@gmail.com>
2011-03-08 04:03:16 -0800

    new branch

--- js/globals.js
+++ js/globals.js
@@ -1,6 +1,6 @@
 
 // constants
-var APP_LOC = "http://www.stanford.edu/~jasonch/cgi-bin/cel/";
+var APP_LOC = "http://www.stanford.edu/~dliddell/cgi-bin/cs247/";
 var AJAX_DIR = "ajax/";
 var APP_ID = '199771060034320';
 var CUR_PAGE = "none"; 
--- js/module_friends.js
+++ js/module_friends.js
@@ -1,4 +1,4 @@
-function load_friends () {
+/*function load_friends () {
 	  FB.api ('/me/friends', function (friends) {
       window.FRIENDS = friends.data;
       updateFriendBlock (function () {return true;});
@@ -36,6 +36,62 @@ function updateFriendBlock (nameFilter) {
         if (count > 20) break;
       }
       $('.friend-item').bind('mouseenter mouseleave', function () {
+        $(this).toggleClass('focused');
+      });
+
+}*/
+
+/*load a random compilation of goals in the box */
+function load_goals () {
+      var goals = [];
+      goals = getGoals();
+      if(goals == -1) return; /* there are no goals yet */
+      updateGoalBlock (goals, function (){return true;} );
+}
+
+function getGoals() {
+  $.ajax ({
+    type: "POST",
+    url: AJAX_DIR + "getGoals.php",
+    data: "user_id=" + USER_INFO.user_id,
+    success: function (data) {
+       var goals = eval ('(' + data + ')');
+        if (goals != undefined)
+          updateGoalBlock (goals, function (){return true;} );
+    }
+  });
+}
+
+
+function formatGoals (goal) {
+  var result = "<div class='goal' id='user-challenge-" + goal.goal_id + "'" +
+  "<ul><li>Goal: " + goal.goal +"</li><li>Friends Involved: " + goal.num_following + "</li></ul></div>";
+  return result;
+}
+
+function updateGoalBlock (goals, nameFilter) {
+	var result = "";
+    var count = 0;
+	for(var i = 0; i < goals.length; i++) {
+		if (!nameFilter (goals[i].goal)) continue;
+		result += formatGoals(goals[i]);
+        count++;
+        if (count > 20) break;
+	}
+	$('#goals-list').html (result);
+
+    for (var i = 0, count = 0; i < goals.length; i++) {
+    if (!nameFilter (goals[i].goal)) continue;
+        var id = goals[i].id;
+        var goal = goals[i].goal;
+        $('#goal-item-' + id).bind ('click', {fid:id, fname: goal}, function (event) {
+          boxOpen ("join-pool",
+            "joinPool.php?from="+USER_INFO.user_id + "&to=" + event.data.fid+"&fname="+event.data.fname, "goal-item-"+ event.data.fid);
+        });
+        count++;
+        if (count > 20) break;
+      }
+      $('.goal-item').bind('mouseenter mouseleave', function () {
         $(this).toggleClass('focused');
       });
 
