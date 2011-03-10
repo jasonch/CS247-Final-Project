12a894664218001fb92a3fe244800f8333ee08d7
Sean Woolfolk <seanw4@myth12.Stanford.EDU>
2011-03-09 23:15:17 -0800

    Fixes addGoal.php

--- ajax/addGoal.php
+++ ajax/addGoal.php
@@ -1,48 +1,48 @@
 <?php
 
 	include ('./connect.php');
-	if (empty ($_POST['from_user']) || empty ($_POST['to_user']) ||
-		empty ($_POST['goal']) || empty ($_POST['num_days'])) {
+	if (empty ($_GET['from_user']) || empty ($_GET['to_user']) ||
+		empty ($_GET['goal']) || empty ($_GET['num_days'])) {
 		echo json_encode(FALSE);
 		echo "Invalid parameters";
 		exit();
 	}
 
-	$user = $_POST['from_user'];
-	$aToUser = $_POST['to_user'];
-	$aGoal = $_POST['goal'];
-	$aNumDays = $_POST['num_days'];
+	$aFromUser = $_GET['from_user'];
+	$aToUser = $_GET['to_user'];
+	$aGoal = $_GET['goal'];
+	$aNumDays = $_GET['num_days'];
 	
 	$names = explode(",", $aToUser);
+	print_r($names);
 
 	$insertGoalQuery = "INSERT INTO goals ('goal', 'num_days') VALUES  ('".$aGoal."','".$aNumDays."')";
 	$getGoalIdQuery = "SELECT goal_id FROM goals ORDER BY time_created LIMIT 1";
 	$getPoolIdQuery = "SELECT goal_id FROM pools ORDER BY time_created LIMIT 1";
 	$index = 0;
+	$countThree = 0;
 	try {
+		$countOne = $db->exec($insertGoalQuery);
+    	$goalId = $db->lastInsertId('goal_id');
+    	$insertPoolQuery = "INSERT INTO pools ('goal_id') VALUES ('".$goalId."')";
+    	$countTwo = $db->exec($insertPoolQuery);
+    	$poolId = $db->lastInsertId('pool_id');
+    	$insertParticipantsQuery = "INSERT INTO participants ('pool_id', 'user_id') VALUES ('".$poolId."', '".$aFromUser."')";
+    	$countThree = $db->exec($insertParticipantsQuery);
 		foreach ($names as $row) {
-    		$db->beginTransaction();
-    	
-    		$countOne = $db->exec($insertGoalQuery);
-    		$goalId = $db->lastInsertId('goal_id');
-    		$insertPoolQuery = "INSERT INTO pools ('goal_id') VALUES ('".$goalId."')";
-    		$countTwo = $db->exec($insertPoolQuery);
-    		$poolId = $db->lastInsertId('pool_id');
-    		$insertParticipantsQuery = "INSERT INTO participants ('pool_id', 'user_id') VALUES ('".$poolId."', '".$aFromUser."');
-    							    INSERT INTO participants ('pool_id', 'user_id') VALUES ('".$poolId."', '".$row[$index]."')";
- 			$countThree = $db->exec($insertParticipantQuery);
- 			if ($countOne != 1 || $countTwo != 1 || $countThree != 2) {
- 				$db->rollback();
- 				echo json_encode(FALSE);
-      			echo "Insert failed. Rolling back.";
-	   			exit();
- 			} else {
- 				echo json_encode(TRUE);	
- 			}
+    		$insertParticipantsQuery = "INSERT INTO participants ('pool_id', 'user_id') VALUES ('".$poolId."', '".$row."')";
+ 			$db->exec($insertParticipantsQuery);
+ 			$countThree++;
  			$index++;
  		}
+ 		if ($countOne != 1 || $countTwo != 1 || $countThree != count($names) + 1 ) {
+ 			echo json_encode(FALSE);
+      		echo "Insert failed. Rolling back.";
+	   		exit();
+ 		} else {
+ 			echo json_encode(TRUE);	
+ 		}
 	} catch (PDOException $e) {
-    	$db->rollBack();
     	echo json_encode(FALSE);
     	echo $e;
 	}
